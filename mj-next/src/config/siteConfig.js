e34546c24371559e97530365b6e1dbba23e561e8
KUMA-Kg <keijikuma001@gmail.com>
2026-02-17 21:39:51 +0900

    Update map embed URL format in siteConfig.js

--- mj-next/src/config/siteConfig.js
+++ mj-next/src/config/siteConfig.js
@@ -33,8 +33,7 @@ export const siteConfig = {
       "しっかりと寄り添い、",
       "理想の住環境づくりをサポートします。",
     ],
-    mapEmbedUrl:
-      "<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d202.15439861289144!2d139.70384031463263!3d35.83746800790241!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzXCsDUwJzE1LjEiTiAxMznCsDQyJzEzLjgiRQ!5e0!3m2!1sja!2sjp!4v1771328023985!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>",
+    mapEmbedUrl:"https://www.google.com/maps?q=35.8375318,139.7038372&z=17&output=embed",
     image: "/images/about_visual_new_v3.png",
   },
 
